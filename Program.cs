57a6321cb137a0cc03d32536d9db3c87deeded0c
AshleyQSmith <smittyq@gmail.com>
2020-05-14 08:50:36 -0600

    book class + constructor functional

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using console_library.Models;
 
 namespace console_library
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+      //   section below = used prior to contructor
+      //   Book myBook = new Book();
+      //   myBook.Author = "Shel Silverstein";
+      //   myBook.Title = "Where the Sidewalk Ends";
+
+      //   section below = usable b/c of constructor
+      Book whereTheSidewalkEnds = new Book("Where the Sidewalk Ends", "Shel Silverstein");
+
     }
+  }
 }
